b9090cca5e12f59eee65fe597bb9c59ba875370b
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-01-28 11:03:47 +0800

    chore: change sidebar name

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -7,8 +7,6 @@
     <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
   </div> -->
   <div class="ml-1 grid flex-1 text-left text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold"
-      >ERP Car Boundary Service</span
-    >
+    <span class="mb-0.5 truncate leading-tight font-semibold">ELISINCPH</span>
   </div>
 </template>
